47a5e9ed4fdb0dd8aa83e16bd19602487c8c03ba
jtrtj <jt.r@live.com>
2018-06-20 15:07:04 -0600

    deck merge sorts values

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,7 @@
+require './lib/merge_sort.rb'
+
 class Deck
+  include MergeSort
   attr_reader :cards
   def initialize(cards)
     @cards = cards
@@ -6,5 +9,19 @@ class Deck
 
   def count
     @cards.length
+  end
+
+  def merge_sort
+    
+  end
+
+  def merge_sort_card_values
+    mergesort(list_of_card_values)
+  end
+  
+  def list_of_card_values
+    @cards.map do |card|
+      card.value.to_i
+    end
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,8 +5,8 @@ require './lib/card.rb'
 
 class DeckTest < MiniTest::Test
   def setup
-    @card_1 = Card.new("3","Hearts")
-    @card_2 = Card.new("4", "Clubs")
+    @card_1 = Card.new("4","Hearts")
+    @card_2 = Card.new("3", "Clubs")
     @card_3 = Card.new("5", "Diamonds")
     @deck = Deck.new([@card_1, @card_2, @card_3])
   end
@@ -17,5 +17,16 @@ class DeckTest < MiniTest::Test
 
   def test_deck_can_be_counted
     assert_equal 3, @deck.count
+  end
+
+  # def test_deck_can_be_merge_sorted
+  #   assert_equal [], @deck.merge_sort
+  # end
+  def test_deck_can_sort_card_values
+    assert_equal [3, 4, 5], @deck.merge_sort_card_values
+  end
+
+  def test_deck_can_make_list_of_card_values
+    assert_equal [4, 3, 5], @deck.list_of_card_values
   end
 end
